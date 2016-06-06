2e6e1a2794f81983aad263f292d9558b9cdf418a
kanjanasak <aundroid08@gmail.com>
2016-06-06 16:45:27 +0700

    ออกแบบ SingUp

---
+++ app/src/main/java/mangmee/kanjanasak/itpbru/SignUpActivity.java
@@ -0,0 +1,15 @@
+package mangmee.kanjanasak.itpbru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }//main
+
+
+} // main class
